76aab1df808156d426ca51b690b4c44e5e74ffaf
MunyReach <munyreachsakal@gmail.com>
2026-03-07 11:19:01 +0700

    I added password to staff and Manager

--- demo/src/main/java/bookstore/Staff.java
+++ demo/src/main/java/bookstore/Staff.java
@@ -1,8 +1,14 @@
 package bookstore;
-
+private String password;
 public class Staff extends User{
     public Staff(String name, String email) {
         super(name, email);
+    }
+public setPassword(String password) {
+        this.password = password;
+    }
+private String getPassword() {
+        return password;
     }
 
     @Override
--- demo/src/main/java/bookstore/User.java
+++ demo/src/main/java/bookstore/User.java
@@ -3,14 +3,20 @@ package bookstore;
 public abstract class User {
     protected String name;
     protected String email;
+    private String password;
 
-    public User(String name, String email) {
+
+    public User(String name, String email, String password)  {
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     public String getName() { return name; }
     public String getEmail() { return email; }
+    private String getPassword() { return password; }
+
+
 
     public abstract String getRole();
 }
