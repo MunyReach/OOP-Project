e1b941c6a6967726eb421fff1203e2486778dfab
Rathanak324 <sereywathrathanak@gmail.com>
2026-03-07 22:19:27 +0700

    Display have done

--- demo/src/main/java/bookstore/App.java
+++ demo/src/main/java/bookstore/App.java
@@ -2,22 +2,20 @@ package bookstore;
 
 import bookstore.MenuRelated.Buying.BuyBooks;
 import java.util.Scanner;
-//simplify app and move scanner 
+
 public class App 
 {
     
     public static void main( String[] args )
     {
-       Scanner scanner = new Scanner(System.in);  // only one scanner
+    Scanner scanner = new Scanner(System.in);
 
         System.out.println("====== Welcome to Bookstore ======");
         
-        BuyBooks buyBooks = new BuyBooks(scanner); // pass scanner to BuyBooks
+        BuyBooks buyBooks = new BuyBooks(scanner);
         buyBooks.displayBooksForSale();
 
-        scanner.close();  // close only at the very end
-
-//delete old if condition because repetitive input in scanner.nextline
+        scanner.close();
 
     }
 }
